d8da041247eee3622b640b2fb22580edd2d90682
sultanmahmud07 <asadkhan01862@gmail.com>
2026-05-03 20:32:27 +0600

    Removed generateMetadata function

--- src/app/news/[slug]/page.js
+++ src/app/news/[slug]/page.js
@@ -2,22 +2,23 @@ import BlogDetails from "../../../components/Pages/Blog/BlogDetails/BlogDetails"
 import TopGap from "../../../components/Shared/TopGap/TopGap";
 import getSingleBlog from "../../../lib/getSingleBlog";
 
-// export async function generateMetadata({ params }) {
-//   const slug = (await params).slug
-//   const blog = await getSingleBlog(slug);
- 
-//   return {
-//     title: blog?.data?.metaTitle || blog?.data?.name_en,
-//     description: blog?.data?.metaDescription,
-//   }
-// }
- 
+export async function generateMetadata({ params }) {
+  const slug = (await params).slug
+  const blog = await getSingleBlog(slug);
 
-export default function Page({params}) {
+  return {
+    title: blog?.data?.metaTitle || blog?.data?.name_en,
+    description: blog?.data?.metaDescription,
+  }
+}
+
+
+export default async function Page({ params }) {
+  const slug = (await params).slug
   return (
     <div className="">
       <TopGap></TopGap>
-      <BlogDetails slug={params?.slug}></BlogDetails>
+      <BlogDetails slug={slug}></BlogDetails>
     </div>
   );
 }
--- src/app/portfolio/[slug]/page.js
+++ src/app/portfolio/[slug]/page.js
@@ -4,12 +4,14 @@ import TopGap from "../../../components/Shared/TopGap/TopGap";
 import getSpecificPortfolioImage from "../../../lib/getSpecificPortfolioImage";
 
 export default async function Page({ params }) {
-  const portfolio = await getSpecificPortfolioImage(params?.slug);
+  const slug = (await params).slug
+  const portfolio = await getSpecificPortfolioImage(slug);
   const locale = await getLocale();
+
   return (
     <div className="">
       <TopGap></TopGap>
-      <ImagePortfolioDetails slug={params?.slug} locale={locale} data={portfolio?.data}></ImagePortfolioDetails>
+      <ImagePortfolioDetails slug={slug} locale={locale} data={portfolio?.data}></ImagePortfolioDetails>
     </div>
   );
 }
